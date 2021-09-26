de76d5da140aa58fac5dc601dc287eb2b0cda7f9
it-step-gomel <52787971+it-step-gomel@users.noreply.github.com>
2021-09-26 23:08:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@
 function changeVideo(url){
     document.getElementById('video-slider').src = url;
 }
+// title focus tabs
+let timer;
+let t = document.querySelector('head title').innerHTML;
+
+function changeTitle(icon, text) {
+    document.querySelector('head title').innerHTML = text;
+    document.querySelector('link[rel="icon"]').setAttribute('href', icon);
+}
+
+window.onblur = () => {
+    timer = setTimeout(() => {
+        changeTitle('./heart.ico', '😭 Вернитесь 😭 ✋');
+    }, 3000)
+}
+
+window.onfocus = () => {
+    clearTimeout(timer);
+    changeTitle('./favicon.ico', t);
+}
